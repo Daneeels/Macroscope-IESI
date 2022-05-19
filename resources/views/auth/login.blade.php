a04c7aee730e959c03d6c173d19938a2c48a00ae
Daneeels <sasongkoindra76@gmail.com>
2022-05-19 14:07:58 +0700

    [ADD] Adding regsiter link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,10 @@
                 </label>
             </div>
 
+            <div class="block mt-4">
+                <a href="{{route('register')}}">Register here</a>
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
