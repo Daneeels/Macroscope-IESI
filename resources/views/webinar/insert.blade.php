31c717758f47467b9cef234d166e57865a2c2d67
User name <you@example.com>
2022-05-21 15:12:13 +0700

    [MOD] fixed insert and edit page for webinar

--- resources/views/webinar/insert.blade.php
+++ resources/views/webinar/insert.blade.php
@@ -1,88 +1,85 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Insert Article
-        </h2>
-    </x-slot>
+        <div class="text-center">
+            <h2 class="font-extrabold text-3xl leading-tight mx-auto my-2">
+                Add Webinar
+            </h2>
+            <h4 class="text-md-center opacity-50">"The right order of words can move mountains" </h4>
+        </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{route('webinar.store')}}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="title" class="form-label">Title</label>
-                            <input type="text" name="title" class="form-control" id="title" placeholder="Insert title here" required value="{{old('title')}}">
-                            @error('title')
-                                <div class="text-danger mt-2">
-                                    {{$message}}
-                                </div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="speaker" class="form-label">Speaker</label>
-                            <input type="text" name="speaker" class="form-control" id="speaker" placeholder="Insert speaker here" required value="{{old('speaker')}}">
-                            @error('speaker')
-                                <div class="text-danger mt-2">
-                                    {{$message}}
-                                </div>
-                            @enderror
-                        </div>
-                        <div class="mb-3 d-flex justify-content-between gap-3">
-    
-                            <div class="w-50">
-                                <label for="link" class="form-label">Link</label>
-                                <input type="text" name="link" class="form-control" id="link" placeholder="Insert link here" required value="{{old('link')}}">
-                                @error('link')
-                                    <div class="text-danger mt-2">
-                                        {{$message}}
-                                    </div>
-                                @enderror
+        <div class="container mx-auto my-16">
+            <div class="p-12">
+                <form action="{{ route('webinar.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-7">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" name="title"
+                            class="block mt-1 w-full rounded-md border-black border-opacity-20" id="title"
+                            placeholder="Insert title here" required value="{{ old('title') }}">
+                        @error('title')
+                            <div class="text-danger mt-2">
+                                {{ $message }}
                             </div>
-    
-                            <div class="w-50">
-                                <label for="date" class="form-label">Date</label>
-                                <input type="date" name="date" id="date" value="{{old('link')}} required" class="form-control">
-                                @error('date')
-                                    <div class="text-danger mt-2">
-                                        {{$message}}
-                                    </div>
-                                @enderror
+                        @enderror
+                    </div>
+                    <div class="mb-7">
+                        <label for="speaker" class="form-label">Speaker</label>
+                        <input type="text" name="speaker"
+                            class="block mt-1 w-full rounded-md border-black border-opacity-20" id="speaker"
+                            placeholder="Insert speaker here" required value="{{ old('speaker') }}">
+                        @error('speaker')
+                            <div class="text-danger mt-2">
+                                {{ $message }}
                             </div>
-                            
-                            
-                        </div>
+                        @enderror
+                    </div>
 
+                    <div class="mb-7 sm:flex mx-auto gap-9">
                         <div class="mb-7">
-                            <div class="flex flex-col">
-                                <div>
-                                    <label for="image" class="form-label">Image</label>
+                            <label for="link" class="form-label">Link</label>
+                            <input type="text" name="link" class="block mt-1 rounded-md border-black border-opacity-20"
+                                id="link" placeholder="Insert link here" required value="{{ old('link') }}">
+                            @error('link')
+                                <div class="text-danger mt-2">
+                                    {{ $message }}
                                 </div>
-    
-                                <div>
-                                    <input type="file" name="image" id="image">
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="date" class="form-label">Date</label>
+                            <input type="date" name="date" id="date" value="{{ old('link') }} required"
+                                class="block mt-1 rounded-md border-black border-opacity-20">
+                            @error('date')
+                                <div class="text-danger mt-2">
+                                    {{ $message }}
                                 </div>
-    
-                                <div>
-                                    @error('image')
-                                        <div class="text-danger mt-2">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="mb-7">
+                        <div class="flex flex-col">
+                            <div>
+                                <label for="image" class="form-label">Image</label>
                             </div>
-                            
+                            <div>
+                                <input type="file" name="image" id="image" class="text-gray-500">
+                            </div>
+                            <div>
+                                @error('image')
+                                    <div class="text-red-500 mt-2">
+                                        *{{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
                         </div>
-    
-                        <div class="d-flex justify-content-center mt-4">
-                            <button type="submit" class="btn btn-dark w-100">Insert</button>
-                        </div>
-                        
-                        
-                    </form>
-                </div>
+                    </div>
+                    <div class="flex justify-end">
+                        <button type="submit"
+                            class="btn btn-dark w-100 bg-black hover:bg-gray-600 text-white p-3 rounded-md">Insert</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
+    </x-slot>
 </x-app-layout>
