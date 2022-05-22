4e7fce88828097ed24f64fcc90880d3bf100f50c
User name <you@example.com>
2022-05-22 20:14:49 +0700

    [MOD] Adding footer in layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="bg-gray-900 text-white">
+    <div class="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 shadow-md">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-2">
+            <!-- Mitra -->
+            <div class="px-3 mb-3 md:col-span-2 lg:col-span-2">
+                <h1 class="text-5xl font-extrabold mb-5">Macroscope</h1>
+                <p>Best clothing store in Malang is launching an educational website. Macroscope
+                    strives to contribute,
+                    in developing education for business enthusiasts around the globe.
+                </p>
+            </div>
+            <!-- Company -->
+            <div class="px-3 mb-3">
+                <h2 class="text-lg font-bold mb-2">Company</h2>
+                <ul class="list-none">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Main Website</a></li>
+                </ul>
+            </div>
+            <!-- Features -->
+            <div class="px-3 mb-3">
+                <h2 class="text-lg font-bold mb-2">Features</h2>
+                <ul class="list-none">
+                    <li><a href="#">Article</a></li>
+                    <li><a href="#">Webinar</a></li>
+                </ul>
+            </div>
+            <!-- Contact Us -->
+            <div class="px-3 mb-3">
+                <h2 class="text-lg font-bold mb-2">Contact Us</h2>
+                <ul class="list-none flex">
+                    <li><a href="#"><img src="../../../icon/socmed_email.svg" alt="email" class="px-1"></a>
+                    </li>
+                    <li><a href="#"><img src="../../../icon/socmed_instagram.svg" alt="instagram"
+                                class="px-1"></a></li>
+                    <li><a href="#"><img src="../../../icon/socmed_twitter.svg" alt="twitter"
+                                class="px-1"></a></li>
+                    <li><a href="#"><img src="../../../icon/socmed_linkedin.svg" alt="linkedin"
+                                class="px-1"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="mt-14 mb-2 flex lg:justify-end md:justify-end justify-center">
+            <h3 class="text-sm font-thin">Copyright @ Macroscope 2022. All Rights Reserved.</h3>
+        </div>
+
+</footer>
